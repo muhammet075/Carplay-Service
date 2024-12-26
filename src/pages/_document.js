3f72b605dcdd2fa8142b9c801874c5a16467c246
muhammet075 <muhammet075@icloud.com>
2024-12-26 03:12:30 +0100

    push

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,18 +2,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="nl">
       <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-WXC5CN7M0Q"></script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QE6B9E1DEY"></script>
       <script
         dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-WXC5CN7M0Q');`
-        }}
-      />
+          gtag('config', 'G-QE6B9E1DEY');`
+        }}/>
       </Head>
       <body>
         <Main />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/assets/logo/carplayservice-logo.svg";
 
 export default function Home() {
   useEffect(() => {
@@ -8,13 +11,22 @@ export default function Home() {
   return (
     <>
       <Head>
-          <title>Next</title>
-          <meta name="description" content=""/>
-          <meta name="og:description" content=""/>
-          <meta name="keywords" content=""/>
-          <meta property="og:url" content=""/>
-          <meta property="og:title" content=""/>
+          <title>Carplay Service | Website in aanbouw</title>
+          <meta name="description" content="Carplay Service - Wij bieden hoogwaardige Carplay-installatie- en integratiediensten. Onze website is momenteel in aanbouw, maar we staan klaar om u te helpen met uw behoeften."/>
+          <meta name="og:description" content="Carplay Service - Onze website is in aanbouw, maar we bieden professionele Carplay-installatie en service voor uw voertuig."/>
+          <meta name="keywords" content="Carplay, Carplay installatie, auto integratie, Carplay Service, auto technologie"/>
+          <meta property="og:url" content="https://www.carplayservice.nl"/>
+          <meta property="og:title" content="Carplay Service | Website in aanbouw"/>
+          <meta property="og:image" content="/oglogo.png"/>
+          <meta property="og:type" content="website"/>
       </Head>
+
+      <div className="aanbouw">
+          <Link href="/">
+              <Image src={Logo} alt="Logo van Carplay Service"/>
+          </Link>
+          <h1>Website in aanbouw...</h1>
+      </div>
     </>
   );
 }
